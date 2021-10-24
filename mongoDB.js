aa57193b827a463d4b7ef0b83cc6524f4d2b85a2
Santhosh Umapathi <Santhosh.Professional1@gmail.com>
2021-10-24 08:30:54 +0200

    FEAT: Insert to MongoDB completed

--- mongoDB.js
+++ mongoDB.js
@@ -1,8 +1,30 @@
 const MongoClient = require("mongodb").MongoClient;
 
-const url = `mmongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_PASSWORD}@playground-cluster.liar9.mongodb.net/starter_proj?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_PASSWORD}@playground-cluster.liar9.mongodb.net/rn-ger-mongodb-starter?retryWrites=true&w=majority`;
 
-const createProduct = async (req, res, next) => {};
+const client = new MongoClient(url);
+const openConnection = async () => await client.connect();
+const closeConnection = () => client.close();
+const db = client.db();
+
+const createProduct = async (req, res, next) => {
+  const { name, price } = req.body;
+  const product = { name, price };
+
+  try {
+    await openConnection();
+
+    const result = await db.collection("products").insertOne(product);
+  } catch (error) {
+    return res.status(422).json({ message: "Product creation failed" });
+  }
+
+  closeConnection();
+
+  return res
+    .status(200)
+    .json({ message: " Product created successfully", product });
+};
 
 const getProducts = async (req, res, next) => {};
 
